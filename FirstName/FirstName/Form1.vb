403855b426a220e608813b45f06bf493f4bbfcef
idsc3101 <idsc3101@gmail.com>
2013-09-16 15:24:27 -0500

    Work Day 2

--- FirstName/FirstName/Form1.vb
+++ FirstName/FirstName/Form1.vb
@@ -4,5 +4,26 @@ Public Class Form1
 
     Private Sub btnFirstName_Click(sender As System.Object, e As System.EventArgs) Handles btnFirstName.Click
 
+        Dim fullname As String
+        ' read the input from the user
+        fullname = txtBox1.Text
+
+        ' find the index of the first space
+        Dim idxSpace As Integer
+        idxSpace = fullname.IndexOf(" ")
+
+        'extract the first name
+        Dim firstName As String
+        firstName = fullname.Substring(0, idxSpace)
+
+
+
+        ' diplay result to user
+        txtFirstName.Text = fullname
+
+
+
+
+
     End Sub
 End Class
