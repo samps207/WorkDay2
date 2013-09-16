becbf5c08edf0402bb7df2f18225e8d4cd58329d
idsc3101 <idsc3101@gmail.com>
2013-09-16 15:25:50 -0500

    Work Day 2

--- FirstName/FirstName/Form1.vb
+++ FirstName/FirstName/Form1.vb
@@ -8,15 +8,17 @@ Public Class Form1
         ' read the input from the user
         fullname = txtBox1.Text
 
+        'Trim the input so as to remove leading spaces
+        Dim fullnameTrimmed As String
+        fullnameTrimmed = fullname.Trim()
+
         ' find the index of the first space
         Dim idxSpace As Integer
-        idxSpace = fullname.IndexOf(" ")
+        idxSpace = fullnameTrimmed.IndexOf(" ")
 
         'extract the first name
         Dim firstName As String
-        firstName = fullname.Substring(0, idxSpace)
-
-
+        firstName = fullnameTrimmed.Substring(0, idxSpace)
 
         ' diplay result to user
         txtFirstName.Text = fullname
